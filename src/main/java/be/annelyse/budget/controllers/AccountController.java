7119e314cd024161ff2e97ab6d86a492fadf393d
Annelies Claes <annelies.claes@vrt.be>
2019-11-09 17:28:37 +0100

    account controller modification according to udemy - spring - petclinic ownercontroller
    html thymeleaf needs to be updated

--- src/main/java/be/annelyse/budget/controllers/AccountController.java
+++ src/main/java/be/annelyse/budget/controllers/AccountController.java
@@ -1,11 +1,17 @@
 package be.annelyse.budget.controllers;
 
 import be.annelyse.budget.commands.AccountCommand;
+import be.annelyse.budget.model.Account;
 import be.annelyse.budget.service.AccountService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 
 @Slf4j //lombok logging to use just by eg. log.debug("I'm in service")
@@ -13,35 +19,99 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class AccountController {
 
+    private static final String VIEWS_ACCOUNT_DETAILS = "accounts/accountDetails";
+    private static final String VIEWS_ACCOUNT_LIST = "accounts/accountsList";
+    private static final String VIEWS_ACCOUNT_CREATE_OR_UPDATE_FORM = "accounts/createOrUpdateAccountForm";
+    private static final String VIEWS_ACCOUNT_FIND = "accounts/findAccounts";
+
     private final AccountService accountService;
 
     public AccountController(AccountService accountService) {
         this.accountService = accountService;
     }
 
-    @GetMapping({"/", "", "/index", "/index.html"})
-    public String listAccounts(Model model) {
-        log.debug("AccountController listAccounts reached");
-        model.addAttribute("accounts", accountService.findAll());
-        return "accounts/index";
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
     }
 
     @GetMapping("/find")
-    public String findAccounts() {
+    public String findAccounts(Model model) {
         log.debug("AccountController findAccounts reached");
-        return "notimplemented";
+        model.addAttribute("account", Account.builder());
+        return VIEWS_ACCOUNT_FIND;
+    }
+
+    @GetMapping()
+    public String processFindForm(Account account, BindingResult result, Model model){
+        //allow parameterless GET request for /accounts to return all records
+        if (account.getName() == null){
+            account.setName(""); //empty string signifies broadest possible search
+        }
+
+        //find accounts by name like
+        List<Account> results = accountService.findAllByNameLike(account.getName());
+
+        if(results.isEmpty()){
+            //no accounts found
+            result.rejectValue("name","notFound", "not found");
+            return VIEWS_ACCOUNT_FIND;
+        } else if (results.size() == 1){
+            // 1 account found
+            account = results.get(0);
+            return "redirect:/accounts/" + account.getId();
+        } else {
+            // multiple accounts found
+            model.addAttribute("accounts", results);
+            return VIEWS_ACCOUNT_LIST ;
+        }
     }
 
     @GetMapping("/{id}")
-    public String toShowId(@PathVariable String id){
-        return "redirect:/accounts/{id}/show";
+    public String showById(@PathVariable Long id, Model model){
+        log.debug("AccountController showById reached: id " + id);
+        model.addAttribute("account", accountService.findById(id));
+        return VIEWS_ACCOUNT_DETAILS;
     }
 
-    @GetMapping("/{id}/show")
-    public String showById(@PathVariable String id, Model model){
-        log.debug("AccountController showById reached: id " + id);
-        model.addAttribute("account", accountService.findById(new Long(id)));
-        return "accounts/show";
+    @GetMapping("/new")
+    public String initCreationForm(Model model){
+        log.debug("AccountController initCreationForm() reached");
+        model.addAttribute("account", Account.builder().build());
+        model.addAttribute("currenciesInUse", accountService.getCurrenciesToChoose());
+        return VIEWS_ACCOUNT_CREATE_OR_UPDATE_FORM;
+    }
+
+    @PostMapping("/new")
+    public String processCreationForm(@Valid Account account, BindingResult result){
+        log.debug("AccountController newAccount() reached");
+        if (result.hasErrors()){
+            return VIEWS_ACCOUNT_CREATE_OR_UPDATE_FORM;
+        } else {
+            Account savedAccount = accountService.save(account);
+            return "redirect:/accounts/"+savedAccount.getId();
+        }
+    }
+
+    @GetMapping("/{id}/edit")
+    public String initUpdateAccountForm(@PathVariable Long id, Model model){
+        log.debug("AccountController initUpdateAccountForm reached: id " + id);
+        model.addAttribute("account", accountService.findById(id));
+        model.addAttribute("currenciesToChoose", accountService.getCurrenciesToChoose());
+        return VIEWS_ACCOUNT_CREATE_OR_UPDATE_FORM;
+    }
+
+    @PostMapping("/{id}/edit")
+    public String processUpdateAccountForm(@Valid Account account, BindingResult result, @PathVariable Long id){
+        log.debug("AccountController processUpdateAccountForm reached: id " + account.getId() );
+
+        if(result.hasErrors()){
+            return VIEWS_ACCOUNT_CREATE_OR_UPDATE_FORM;
+        } else{
+            account.setId(id);
+            Account savedAccount = accountService.save(account);
+            return "redirect:/accounts/" + savedAccount.getId();
+        }
     }
 
     @GetMapping("/{id}/delete")
@@ -52,30 +122,6 @@ public class AccountController {
         } catch (RuntimeException re) {
             log.warn(re.getMessage());
         }
-
         return "redirect:/accounts";
-    }
-
-    @GetMapping("/new")
-    public String newAccount(Model model){
-        log.debug("AccountController newAccount() reached");
-        model.addAttribute("account", new AccountCommand());
-        model.addAttribute("currenciesInUse", accountService.getCurrenciesToChoose());
-        return "accounts/form";
-    }
-
-    @GetMapping("/{id}/update")
-    public String updateAccount(@PathVariable String id, Model model){
-        log.debug("AccountController updateAccount reached: id " + id);
-        model.addAttribute("account", accountService.findCommandById(Long.valueOf(id)));
-        model.addAttribute("currenciesToChoose", accountService.getCurrenciesToChoose());
-        return  "accounts/form";
-    }
-
-    @PostMapping("/saveOrUpdate")
-    public String saveOrUpdate(@ModelAttribute AccountCommand command){
-        log.debug("AccountController saveOrUpdate reached: id " + command.getId() );
-        AccountCommand savedCommand = accountService.saveCommand(command);
-        return "redirect:/accounts/" + savedCommand.getId();
     }
 }
